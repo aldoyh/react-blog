8cbb81d0e1729eb7c448204503f25cc892b0fb2c
Hasan AlDoy <aldoyh@gmail.com>
2024-04-25 23:52:20 +0300

    Updates

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import MainLayout from "../components/MainLayout";
+
+const HomePage = () => {
+  return (
+    <MainLayout>
+      <h1>Home Page</h1>
+    </MainLayout>
+  );
+};
+
+export default HomePage;
